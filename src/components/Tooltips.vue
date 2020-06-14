e5af8a2a7ee496d800b0108c19ce7e8f20daaa37
nikita turabov <nikitaturabov@mail.ru>
2020-06-14 03:40:35 +0300

    Добавлена страница с Тултипами. Добавлен компонент Тултип - CfTooltip

--- src/components/Tooltips.vue
+++ src/components/Tooltips.vue
@@ -1,8 +1,36 @@
 <template>
-	<div>Это вторая страница</div>
+	<div>
+		<h1 class="ds-current-page__title ds-title ds-title--1">
+			TOOL
+			<strong>TIPS</strong>
+		</h1>
+
+		<span
+			class="ds-current-page__text ds-current-page__text--gray"
+		>Отображение информации подсказки для наведения мыши.</span>
+
+		<div class="ds-component">
+			<h3 class="ds-component__name">Туллтип на кнопке</h3>
+			<div class="ds-component__tabs">
+				<button class="ds-component__tab ds-component__tab--active">Square</button>
+			</div>
+			<div class="ds-component__row">
+				<tooltip>
+					<btn element="button" color="red" size="large" text="Войти" shape="round"></btn>
+				</tooltip>
+
+				<tooltip>
+					<cf-input placeholder="Input with tooltip"></cf-input>
+				</tooltip>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import Tooltip from "@/components/tooltip/Tooltip.vue";
+import Btn from "@/components/btns/Btn.vue";
+import CfInput from "@/components/forms/CfInput.vue";
 export default {
 	name: "Tolltips",
 	props: {
@@ -10,6 +38,11 @@ export default {
 	},
 	mounted() {
 		console.log(this.nameBtn);
+	},
+	components: {
+		Btn,
+		Tooltip,
+		CfInput,
 	},
 };
 </script>
---
+++ src/components/tooltip/Tooltip.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="tooltip">
+		<slot class="tooltip__curr-element"></slot>
+		<div class="tooltip__container">
+			<span
+				class="tooltip__text"
+			>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur recusandae, ut odit dolor quibusdam dolorem distinctio autem numquam vel, incidunt aperiam qui perspiciatis officia ad quo omnis? Fugiat, ipsum rerum.</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.tooltip {
+	position: relative;
+	&__container {
+		position: absolute;
+		top: calc(100% + 16px);
+		left: 0;
+		max-width: 262px;
+		background: #3b3e47;
+		border-radius: 4px;
+		padding: 16px;
+		visibility: hidden;
+		opacity: 0;
+		transition: 0.2s;
+
+		&:after {
+			content: "";
+
+			background: #3b3e47;
+			width: 14px;
+			height: 14px;
+			transform: rotate(45deg);
+			position: absolute;
+			top: -6px;
+			left: 16px;
+		}
+	}
+
+	&__text {
+		font-size: 12px;
+		line-height: 16px;
+		letter-spacing: 0.02em;
+		color: #ffffff;
+	}
+
+	&:hover {
+		.tooltip__container {
+			visibility: visible;
+			opacity: 1;
+			transition: 0.2s;
+		}
+	}
+}
+</style>
